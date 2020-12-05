2debbdb1b17e1589ca17e27a4070fa86a7e0be21
yamaorii <yamaorii@gmail.com>
2020-12-05 22:24:36 +0900

    #2 再インストールに対応できてるはず版（コミット後に評価する

--- bin/setup_termux.sh
+++ bin/setup_termux.sh
@@ -5,39 +5,50 @@
 
 # 初期化チェック
 if [ ! -d ~/storage ]; then # まだなら実施
-  echo Termuxが初期化されていません
+  echo [Termuxが初期化されていません]
   echo 以下のコマンドを入力してパッケージを最新化してください
   echo "  pkg update -y"
   echo 以下のコマンドを入力してストレージを初期化してください
   echo "  termux-setup-storage"
   exit 1
 fi
+if [ -d ~/kyoryojoe ]; then # あれば確認
+  echo [KyoRyoJoeがインストールされています]
+  echo 再インストールすると点検データは削除されます
+  echo 同期が必要な場合、中断して下さい
+  echo 続行しますか？
+  read -p " (y/n):" INPUT
+  case "$INPUT" in
+    [yY] ) $(rm -f -d -r ~/kyoryojoe);;
+    * ) exit 1 ;;
+  esac
+fi
 
 # 環境設定
 cd #~
 
-echo Gitをインストールします
+echo [Gitをインストールします]
 echo 成功するとバージョンを表示します
 pkg list-all | grep git # これやると成功率が上がる？
 pkg install git -y # 失敗することあり（もう一度試す
 git --version
 
-echo RubyとBundlerをインストールします
+echo [RubyとBundlerをインストールします]
 echo 成功するとバージョンを表示します
 pkg list-all | grep ruby # これやると成功率が上がる？
 pkg install ruby -y # 失敗することあり（もう一度試す
-ruby -v;
+ruby -v
 gem install bundler
 bundle -v
 
-echo Vimをインストールします
+echo [Vimをインストールします]
 echo 成功するとバージョンを表示します
 pkg list-all | grep vim # これやると成功率が上がる？
 pkg install vim -y # 失敗することあり（もう一度試す
 vim --version
 
 # アプリインストール
-echo KyoRyoJoeをインストールします
+echo [KyoRyoJoeをインストールします]
 git clone https://github.com/kyoryojoe/app.git kyoryojoe
 cd kyoryojoe/
 bundle install
@@ -46,14 +57,14 @@ bundle install
 cd #~
 if [ ! -f ~/.bashrc ]; then # まだなら実施
   echo '#! $PREFIX/bin/bash' >>  ~/.bashrc
+  echo '#KyoRyoJoe start' >> ~/.bashrc
+  echo 'cd ~/kyoryojoe/' >> ~/.bashrc
+  echo 'bundle exec ruby app.rb &' >> ~/.bashrc
+  echo 'echo KyoRyoJoeが起動しました' >> ~/.bashrc
+  echo 'cd -' >> ~/.bashrc
   chmod +x ~/.bashrc
 fi
-echo '#KyoRyoJoe start' >> ~/.bashrc
-echo 'cd ~/kyoryojoe/' >> ~/.bashrc
-echo 'bundle exec ruby app.rb &' >> ~/.bashrc
-echo 'echo KyoRyoJoeが起動しました' >> ~/.bashrc
-echo 'cd -' >> ~/.bashrc
 
 # おわり
-echo セットアップが完了しました
+echo [セットアップが完了しました]
 echo Termuxを再起動してください
